d4fe04491c62b353fd371c3e5bde6cbd405562a0
jaspresso <jaspresso@gmail.com>
2024-10-21 21:34:07 +0900

    git-bash에서 작업해봄

--- javaProject/src/ch01/Hello.java
+++ javaProject/src/ch01/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
 		System.out.println("깃 테스트용 추가");// Git 확인용
 		// 예를 들어 다른 개발자가 아래처럼 소스를 수정했음.
 		System.out.println("B개발자의 소스 수정");
+		System.out.println("내가 다시 소스를 수정");
 	}
 }
