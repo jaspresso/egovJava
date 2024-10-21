c0a8ffae93665fb008572229744ba7481d4653a9
jaspresso <42950313+jaspresso@users.noreply.github.com>
2024-10-21 21:08:21 +0900

    Update Hello.java

--- javaProject/src/ch01/Hello.java
+++ javaProject/src/ch01/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요. 자바 World");
 		System.out.println("깃 테스트용 추가");// Git 확인용
+		// 예를 들어 다른 개발자가 아래처럼 소스를 수정했음.
+		System.out.println("B개발자의 소스 수정");
 	}
 }
